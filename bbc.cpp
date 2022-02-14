247dccd8c4507c5902f420be5b3c62040797e79f
noxius <rohullahalavi@gmail.com>
2022-02-14 19:14:03 +0330

    add asembly files new

---
+++ bbc.cpp
@@ -0,0 +1,21 @@
+
+//simpe hello world app c++
+
+
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
+
+
+//g++ hello.cpp -o hello
+//./hello
+//g++ hello.cpp -o hello -std=c++11
+//./hello
+//g++ hello.cpp -o hello -std=c++14
+//./hello
+//
